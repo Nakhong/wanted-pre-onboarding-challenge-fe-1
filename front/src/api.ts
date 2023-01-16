de8aec4f751aaadbb99c3c41ae882b1294782e6a
HwanakHong <94178576+Nakhong@users.noreply.github.com>
2023-01-17 01:09:57 +0900

    refactor : header에 토큰 기본값으로 추가

--- front/src/api.ts
+++ front/src/api.ts
@@ -1,40 +1,32 @@
 import axios from "axios";
 import { Todo } from "./types/type";
 
-export const baseAxios = axios.create({
+const baseAxios = axios.create({
   baseURL: "http://localhost:8080",
-  // withCredentials: true,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
 });
 
-export async function createTodo(todo: Record<string, string>) {
-  return baseAxios.post("/todos", todo, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-  });
+async function createTodo(todo: Record<string, string>) {
+  return baseAxios.post("/todos", todo);
 }
 
-export async function deleteTodo(id: string) {
-  return await baseAxios.delete(`/todos/${id}`, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-  });
+async function deleteTodo(id: string) {
+  return await baseAxios.delete(`/todos/${id}`);
 }
 
-export async function getTodos() {
-  return await baseAxios.get("/todos", {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-  });
+async function getTodos() {
+  return await baseAxios.get("/todos");
 }
 
-export async function updateTodos(data: Todo) {
+async function getTodosId() {
+  return await baseAxios.get("/todos");
+}
+
+async function updateTodos(data: Todo) {
   const total = { title: data.title, content: data.content };
-  return await baseAxios.put(`/todos/${data.id}`, total, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-  });
+  return await baseAxios.put(`/todos/${data.id}`, total);
 }
+
+export { baseAxios, createTodo, deleteTodo, getTodos, updateTodos, getTodosId };
