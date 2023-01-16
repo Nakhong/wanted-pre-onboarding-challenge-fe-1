c2abc3283e8159dee153913c8a3809d55b38fd01
HwanakHong <94178576+Nakhong@users.noreply.github.com>
2023-01-17 01:10:15 +0900

    feat : react-query

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        {/* <ReactQueryDevtools initialIsOpen={true} /> */}
+        <App />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>
 );
