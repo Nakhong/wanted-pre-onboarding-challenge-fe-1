4883b875be70d455d8be1a822430350de3d5566f
HwanakHong <94178576+Nakhong@users.noreply.github.com>
2023-02-09 17:52:49 +0900

    asdf

--- front/src/util/axios.ts
+++ front/src/util/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const baseAxios = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL:
+    "https://port-0-wanted-pre-onboarding-challenge-be-1-luj2cldwuihed.sel3.cloudtype.app/",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
