996ecb1af86a140f75e2b8b7a4f5ccad0fd983e5
HwanakHong <94178576+Nakhong@users.noreply.github.com>
2023-02-09 19:03:28 +0900

    fix : 주소변경

--- front/src/util/axios.ts
+++ front/src/util/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseAxios = axios.create({
   baseURL:
-    "https://port-0-wanted-pre-onboarding-challenge-be-1-luj2cldwuihed.sel3.cloudtype.app/",
+    "https://port-0-wanted-pre-onboarding-challenge-be-1-luj2cldwuihed.sel3.cloudtype.app",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
